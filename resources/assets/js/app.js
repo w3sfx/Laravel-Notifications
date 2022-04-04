e5f46d91d637e2e2d2f0bbe093c31b56ca282dbe
Wesley Victor <w3sleyv1ctor@gmail.com>
2022-04-04 14:51:07 -0300

    Exibição de Notificações / Organização de Componentes Vue.js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -13,6 +13,7 @@ window.Vue = require('vue');
  * Components
  */
 Vue.component('notifications', require('./components/notifications/Notifications'));
+Vue.component('notification', require('./components/notifications/Notification'));
 //Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
 const app = new Vue({
