231c91250b60eefc97732ab4b7e9539d6e8b65dc
Wesley Victor <w3sleyv1ctor@gmail.com>
2022-04-07 17:49:35 -0300

    Adicionado ao Vuex / Exibição de Alerta / Curso Concluído

--- resources/assets/js/Echo.js
+++ resources/assets/js/Echo.js
@@ -1,7 +1,23 @@
+import store from './vuex/store'
+import swal from 'sweetalert'
+
+const typesNotifications = {
+    postCommented: 'App\\Notifications\\PostCommented',
+}
+
 if (Laravel.user) {
-    console.log(`App.Models.User.${Laravel.user}`)
     Echo.private(`App.Models.User.${Laravel.user}`)
-    .notification(notification => {
-        console.log(notification)
+        .notification(notification => {
+        if (Laravel.user == notification.data.comment.post.user.id){
+            if (notification.type == typesNotifications.postCommented) {
+                swal(
+                    'Novo Comentário',
+                    `O post ${notification.data.comment.post.title} foi comentado por: ${notification.data.comment.user.name}`,
+                    'info'
+                )
+
+                store.commit('ADD_NOTIFICATION', notification)
+            }
+        }
     })
 }
